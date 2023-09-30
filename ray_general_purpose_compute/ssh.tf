0035feca6040d11f9d17e3ccc3986a6c7706388f
Prabodh Agarwal <prabodh1194@gmail.com>
2023-09-30 20:30:53 +0530

    set an egress port

--- ray_general_purpose_compute/ssh.tf
+++ ray_general_purpose_compute/ssh.tf
@@ -17,4 +17,12 @@ resource "aws_security_group" "ssh_sg" {
     self = true
   }
 
+  egress {
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 }
