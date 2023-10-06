152509b508b2dcebd9792c3dc55f2b2c5473637c
Prabodh Agarwal <prabodh1194@gmail.com>
2023-10-06 17:59:26 +0530

    restore spot

--- ray_general_purpose_compute/ec2.tf
+++ ray_general_purpose_compute/ec2.tf
@@ -22,7 +22,14 @@ resource "aws_instance" "ray_general_purpose_machine" {
     instance_type = var.instance_type
     user_data_replace_on_change = true
 
-    user_data = "$(file(setup_ray.sh))"
+    user_data = base64encode("$(file(setup_ray.sh))")
+    instance_market_options {
+        market_type = "spot"
+        spot_options {
+            spot_instance_type = "persistent"
+            instance_interruption_behavior = "stop"
+        }
+    }
 
     security_groups = [
         aws_security_group.ssh_sg.name
